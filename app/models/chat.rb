2da7639cb12627afd6acffaf418f663eccd294b1
aya hussein <aya.hussein3107@gmail.com>
2022-05-03 03:46:24 +0200

    update chat, message count

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,11 +3,18 @@ class Chat < ApplicationRecord
   has_many :messages,dependent: :destroy
 
   before_create :generate_number
-  
+  after_create :update_chat_count
+ 
   protected
   def generate_number
     self.number =  Chat.count == 0? 1 : Chat.maximum("number")+1
     # self.number = Chat.count+1
+  end
+
+  protected
+  def update_chat_count
+    self.application.chat_count += 1
+    self.application.save()
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,16 @@
 class Message < ApplicationRecord
   belongs_to :chat
   before_create :generate_number
-  # after_create :update_msg_count
+  after_create :update_msg_count
   
   protected
   def generate_number
     self.number =  Message.count == 0 ? 1 : Message.maximum("number")+1
   end
 
-  
+  protected
+  def update_msg_count
+    self.chat.message_count += 1
+    self.chat.save()
+  end
 end
